2e16c26601a8addf959800aa454325106146ee76
Davood <davood.akrami.rk2025@gmail.com>
2025-05-01 00:28:48 +0330

    Adding gh pages ability

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfilio/',
+  base: '/portfolio/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
