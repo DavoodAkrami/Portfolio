376e3166965cc53982433b0eb98e1f36036d7888
DavoodAkrami <davood.akrami.rk2025@gmail.com>
2025-08-14 00:58:46 +0330

    Adding direction to davoodakrami.ir

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './Pages/Home/Home';
 
 function App() {
 
-  if (window.location.hostname === "portfolio-one-azure-17.vercel.app/") {
+  if (window.location.href === "https://portfolio-one-azure-17.vercel.app/") {
     window.location.href = "https://davoodakrami.ir";
   }
   return (
