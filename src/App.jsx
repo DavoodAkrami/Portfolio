5fcb013207a3d31893ca63637186a3abe7ab236a
DavoodAkrami <davood.akrami.rk2025@gmail.com>
2025-08-13 20:53:21 +0330

    Adding direction to my main portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from './Pages/Home/Home';
 
 function App() {
 
+  if (window.location.hostname === "https://portfolio-one-azure-17.vercel.app/") {
+    window.location.href = "https://davoodakrami.ir";
+  }
   return (
     <div>
       <Home />
